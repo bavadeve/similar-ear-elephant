64d083d439c2a999d4a7854684f6e805ca9b18d6
Bauke van der Velde <b.vandervelde@uu.nl>
2018-08-30 11:41:01 +0200

    standard functions to create necessary files for automatic folder structure

--- bv_createSetPaths.m
+++ bv_createSetPaths.m
@@ -1,1 +1,46 @@
-function bv_createSetPaths(rootPath, homePath)
+function filecreated = bv_createSetPaths(path2root, path2home)
+
+fid = fopen([path2root filesep 'setPaths.m'], 'w');
+
+fprintf(fid, '%% set all the paths necessary for analysis. Change according to your own \n%% needs. Save this file to your PATHS.ROOT folder so it can always be found \n%% by matlab during analysis\n');
+fprintf(fid, '\n%% Paths to already existing folders\n');
+fprintf(fid, 'PATHS.ROOT = ''%s''; %% analyses folder (subject & figure folders will be created here) \n', path2root);
+fprintf(fid, 'PATHS.HOME = ''%s''; %% path to home folder (where you can find RAWS and PRPEROC usually)\n', path2home);
+fprintf(fid, 'PATHS.RAWS = ''%s''; %% path to the directory of raw EEG files \n', [path2home filesep 'RAW']);
+fprintf(fid, 'PATHS.SUBFUNCTIONS = ''%s''; %% path to your subfunctions directory (place the functions made specifically for your dataset here e.g. trialfun) \n', [path2root filesep 'subfunctions']);
+fprintf(fid, 'PATHS.FILES = ''%s'';\n', [path2home filesep 'files']);
+fprintf(fid, 'PATHS.PREPROC = ''%s''; %% path to the preproc folder \n', [path2home filesep 'PREPROC']);
+fprintf(fid, '\n %% Paths to folders to be created\n');
+path2subjects = [path2root filesep 'Subjects'];
+fprintf(fid, 'PATHS.SUBJECTS = ''%s''; %% path to subject directory\n', [path2subjects]);
+fprintf(fid, 'PATHS.RESULTS = ''%s''; %% path to results folder\n', [path2root filesep 'results']);
+fprintf(fid, 'PATHS.REMOVED = ''%s''; %% path to removed subjects folder \n', [path2subjects filesep 'removed']);
+fprintf(fid, 'PATHS.FIGURES = ''%s''; %% path to figures  folder \n', [path2root filesep 'figures']);
+fprintf(fid, 'PATHS.CONFIG = ''%s''; %% path to config folder \n', [path2root filesep 'config']);
+
+fprintf(fid,'\n%% create, if necessary the folders \nif ~exist(PATHS.SUBJECTS, ''dir'')\n\tmkdir(PATHS.SUBJECTS)\nend\n');
+fprintf(fid,'if ~exist(PATHS.RESULTS, ''dir'')\n\tmkdir(PATHS.RESULTS)\nend\n');
+fprintf(fid,'if ~exist(PATHS.FIGURES, ''dir'')\n\tmkdir(PATHS.FIGURES)\nend\n');
+fprintf(fid,'if ~exist(PATHS.SUBFUNCTIONS, ''dir'')\n\tmkdir(PATHS.SUBFUNCTIONS)\nend\n');
+fprintf(fid,'if ~exist(PATHS.FILES, ''dir'')\n\tmkdir(PATHS.FILES)\nend\n');
+fprintf(fid,'if ~exist(PATHS.REMOVED, ''dir'')\n\tmkdir(PATHS.REMOVED)\nend\n');
+fprintf(fid,'if ~exist(PATHS.CONFIG, ''dir'')\n\tmkdir(PATHS.CONFIG)\nend\n');
+
+fprintf(fid, '\n%% add fieldtrip and the subfunctions to your matlab path\nPATHS.FTPATH = ''/Users/bauke/MatlabToolboxes/fieldtrip-20180209'';');
+fprintf(fid, 'addpath(PATHS.FTPATH)\n');
+fprintf(fid, 'addpath(PATHS.SUBFUNCTIONS)\n');
+fprintf(fid, 'addpath(pwd)\n');
+fprintf(fid, 'addpath(PATHS.FTPATH)\n');
+fprintf(fid, 'addpath(PATHS.FILES)\n');
+fprintf(fid, 'ft_defaults\n');
+
+if exist([path2root filesep 'setPaths.m'], 'file') == 2
+    filecreated = true(1);
+else
+    filecreated = false(1);
+    warning('No setOptions.m file created at given location')
+end
+    
+
+fclose(fid);
+
---
+++ bv_createSubjectFolders_standard.m
@@ -0,0 +1,154 @@
+function bv_createSubjectFolders_standard(cfg, varargin)
+% bv_createSubjectFolders creates the folder structure necessary to run
+% analyses for infant EEG and adds an individual Subject.mat file in each
+% folder with individuals information and paths to important files.
+% Currently set-up to find the following datatype: 'bdf', 'eeg'. Set used
+% datatype in your setOptions file. Creates subject folders with a name based
+% on the raw data filename, by using as foldername the  part of the
+% filename before the first dash ('_'). f.e. 'A12345_raw.bdf' becomes
+% subjectname: 'A12345'
+%
+% Use as:
+%  bv_createSubjectFolders(cfg)
+%
+% The input argument cfg is a configuration structure, which contains all
+% details for the create subject folders.
+%
+% Possible inputarguments in configuration structure:
+%   cfg.optionsFcn      = 'string', filename to options m-file. (default:
+%                           'setOptions'). For example see
+%                           setOptions_empty.
+%   cfg.pathsFcn        = 'string', filename to paths m-file. (default:
+%                           'setPaths'). For example see
+%                           setPaths_empty.
+%
+% Saves an Subject.mat file, with a subjectdata structure with the
+% following fields:
+%   subjectdata.subjectName         = 'string', given name of the subject
+%   subjectdata.date                = 'string', time and data of creation
+%   subjectdata.filename            = 'string', filename of dataset
+%
+%   subjectdata.PATHS               = .structure., with paths to all important
+%                                       files
+%   subjectdata.PATHS.SUBJECTDIR    = 'string', path to subjectdirectory
+%   subjectdata.PATHS.DATAFILE      = 'string', path to dataset
+%   subjectdata.PATHS.HDRFILE       = 'string', path to headerfile
+%
+%
+% Copyright (C) 2015-2017, Bauke van der Velde
+%
+% See also SETPATHS_EMPTY, SETOPTIONS_EMPTY
+
+% read out from configuration structure
+optionsFcn = ft_getopt(cfg, 'optionsFcn','setOptions');
+pathsFcn = ft_getopt(cfg, 'pathsFcn','setPaths');
+inputStr = ft_getopt(cfg, 'inputStr', 'preproc');
+rawdelim = ft_getopt(cfg, 'rawdelim');
+sfolderstruct = ft_getopt(cfg, 'subjectfolderstructure');
+
+% load in standard options and paths
+eval(optionsFcn);
+eval(pathsFcn);
+
+% detect raw eeg files based on path to raws and the subject search string
+% (OPTIONS.sDirString)
+if strcmpi(OPTIONS.dataType, 'mat')
+    files = dir([PATHS.PREPROC filesep '*' inputStr '.mat']);
+    if isempty(files)
+        error('no files found for inputstring: %s \n', inputStr)
+    end
+else
+    files = dir([PATHS.RAWS filesep '*' OPTIONS.sDirString '*']);
+end
+fileNames = {files.name};
+
+
+
+if ~exist(PATHS.SUBJECTS,'dir'); mkdir PATHS.SUBJECTS; end % create, if necessary, Subject folder
+
+removeIdx = 0;
+for subjIndex = 1:length(fileNames)
+    inputnames = varargin;
+    [~, cFile] = fileparts(fileNames{subjIndex});
+    
+    splitFile = strsplit(cFile, '_');
+    
+    inputdiff = length(splitFile) - length(inputnames);
+    if inputdiff > 0
+        inputnames = cat(2,inputnames, cell(1,inputdiff));
+    elseif inputdiff < 0
+        inputnames = inputnames(1:length(inputnames)+inputdiff);
+    end
+    
+    splitFile = splitFile(not(cellfun(@isempty, inputnames)));
+    inputnames = inputnames(not(cellfun(@isempty, inputnames)));
+    
+    for i = 1:length(splitFile)
+        subjectdata.(inputnames{i}) = splitFile{i};
+    end
+    
+    switch OPTIONS.dataType % find raw EEG files (can be 'bdf' or 'eeg' datatype')
+        case 'eeg'
+            israw = 1;
+            dataFile = [PATHS.RAWS filesep cFile  '.eeg'];
+            hdrFile = [PATHS.RAWS filesep cFile  '.vhdr'];
+            
+            if ~exist(dataFile, 'file')
+                error('dataFile: %s not found!', dataFile)
+            elseif ~exist(hdrFile, 'file')
+                error('headerfile: %s not found!', hdrFile)
+            end
+            
+            
+        case 'bdf'
+            israw = 1;
+            dataFile = [PATHS.RAWS filesep cFile  '.bdf'];
+            hdrFile = [PATHS.RAWS filesep cFile  '.bdf'];
+            
+            if ~exist(dataFile, 'file')
+                error('dataFile: %s not found!', dataFile)
+            elseif ~exist(hdrFile, 'file')
+                error('headerfile: %s not found!', hdrFile)
+            end
+            
+            
+        case 'mat'
+            israw = 0;
+            dataFile = 'unknown';
+            hdrFile = 'unknown';
+            
+            subjectdata.PATHS.(upper(inputStr)) = ...
+                [PATHS.PREPROC filesep cFile '.mat'];
+            
+            if ~exist(subjectdata.PATHS.(upper(inputStr)), 'file')
+                error('dataFile: %s not found!', dataFile)
+            end
+            
+            
+        otherwise
+            error(sprintf('unknown datatype %s', OPTIONS.dataType));
+    end
+    
+    subjectdata.subjectName = strjoin(splitFile(ismember(inputnames, sfolderstruct)), rawdelim);
+    disp(subjectdata.subjectName);
+    
+    paths2SubjectFolder = [PATHS.SUBJECTS filesep subjectdata.subjectName]; % create a path to current subject folder
+    if ~exist(paths2SubjectFolder,'dir')
+        mkdir(paths2SubjectFolder); % create, if necessary, individual subject folder
+    end
+    
+    subjectdata.PATHS.SUBJECTDIR = paths2SubjectFolder; % save path to subject folder in subjectdata
+    
+    subjectdata.PATHS.DATAFILE = dataFile; % save both dataset and hdrfile to subjectdata structures
+    subjectdata.PATHS.HDRFILE = hdrFile;
+    [~, subjectdata.filename, ~] = fileparts(subjectdata.PATHS.DATAFILE);
+    
+    subjectdata.date = date;
+    
+    fprintf('\t saving Subject.mat...')
+    save([subjectdata.PATHS.SUBJECTDIR filesep 'Subject'],'subjectdata'); % save individual subjectdata structure to individual folder
+    fprintf('done \n')
+    clear subjectdata
+    
+end
+
