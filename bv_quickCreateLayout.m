e95afae2528f9e6a8be52a66e04dc1b85a9b5650
Bauke van der Velde <b.vandervelde@uu.nl>
2018-09-12 11:37:20 +0200

    creates quick layout

---
+++ bv_quickCreateLayout.m
@@ -0,0 +1,10 @@
+function lay = bv_quickCreateLayout(labels)
+
+cfg = [];
+cfg.layout = 'EEG1010';
+cfg.channel = labels;
+cfg.feedback = 'no';
+cfg.skipcomnt = 'yes';
+cfg.skipscale = 'yes';
+
+lay = ft_prepare_layout(cfg);
