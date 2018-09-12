a39fbaa69018a51cb8cc226cf9afe0aa0071606e
Bauke van der Velde <b.vandervelde@uu.nl>
2018-09-12 11:32:43 +0200

    changed the randomization process to ensure 5 randomizations & removed percentage counter

--- bv_randomizeWeightedMatrices.m
+++ bv_randomizeWeightedMatrices.m
@@ -5,20 +5,19 @@ counter = 0;
 n = size(Ws,3);
 Wrandomized = zeros([size(Ws,1) size(Ws,2) n m]);
 rng(100000)
-for iW = 1:n
-    currW = Ws(:,:,iW);
-
-    weights = squareform(currW);
-    I = find(weights > 0);
-    
-    lng = printPercDone(n*m, counter);
-    for j = 1:m
-        weights(I) = weights(I(randperm(numel(I))));
-        Wrandomized(:,:,iW,j) = squareform(weights);
+for iS = 1:n
+    currW = Ws(:,:,iS);
+    for iW = 1:m
         
-        counter = counter + 1;
-    end  
-    fprintf(repmat('\b', 1, lng))
+        
+        weights = squareform(currW);
+        I = find(weights > 0);
+        
+        for j = 1:5
+            weights(I) = weights(I(randperm(numel(I))));
+            I = find(weights > 0);
+            counter = counter + 1;
+        end
+        Wrandomized(:,:,iS,iW) = squareform(weights);
+    end
 end
-
-fprintf('\n')
