a5336a9d9141d68bc2287c0b716392430d0e97db
Bauke van der Velde <b.vandervelde@uu.nl>
2018-09-25 14:00:48 +0200

    added help doc

--- bv_calculateConnectivity.m
+++ bv_calculateConnectivity.m
@@ -1,21 +1,72 @@
-function connectivity = bv_calculateConnectivity(cfg, data)
+function [connectivity] = bv_calculateConnectivity(cfg, data)
+% Calculates connectivity between EEG sensors with the following methods
+% implemented (pli, wpli, wpli_debiased). Weighted PLI measures are
+% calculated based on fieldtrip ft_connectivityanalysis. PLI is calculated
+% using the PLI.m function included.
+%
+% can be used with data-input as:
+%  [ connectivity ] = bv_calculateConnectivity(cfg, data)
+%
+% with input data being a standard fieldtrip eeg-data-structure
+%
+% or without data-input as:
+%  [ connectivity ] = bv_calculateConnectivity(cfg)
+%
+% always needs the following fields in the cfg structure:
+%   cfg.method      = 'string', connectivity will be calculated based on
+%                       given method ('wpli_debiased', 'wpli', 'pli')
+%   cfg.ntrials     = [ number ] of trials to be randomly included in
+%                       connectivity calculation. Can be a number or 'all'
+%                       to include all trials (default: 'all')
+%   cfg.condition   = [ number ], number of condition to be analyzed (taken
+%                       from data.trialinfo. Can also be 'all' to include
+%                       all conditions (default: 'all');
+%
+% also needs the following field in cfg-structure in the case of no data-input:
+%   cfg.currSubject = 'string', subjectfoldername to analyze (looks in
+%                       PATHS.SUBJECTS given in cfg.pathsFcn given)
+%   cfg.inputStr    = 'string', data to be loaded in, using bv_check4data,
+%                       based on subjectdata.PATHS field name (f.e.
+%                       'APPENDED')
+%   cfg.saveData    = 'yes/no', determines whether data is saved to disk in
+%                       subjectfolder, based on cfg.outputStr with
+%                       bv_saveData
+%   cfg.outputStr   = 'string', used to save data to unique file, with
+%                       unique entry into subjectdata.PATHS
+%   cfg.pathsFcn    = path to paths function needed to run
+%                       analysis-pipeline (default: './setPaths')
+%   cfg.optionsFcn  = path to options function needed to run
+%                       analysis-pipeline (default: './setOptions')
+%
+% optional fields:
+%   cfg.triallength = [ number ], cut data into trials with given length,
+%                       using bv_cutAppendedIntoTrials. Leave empty to
+%                       leave the trialstructure of input file
+%
+% See also BV_CREATESUBJECTFOLDERS, BV_CUTAPPENDEDINTOTRIALS,
+% BV_SAVEDATA, BV_CHECK4DATA, FT_CONNECTIVITYANALYSIS, BV_SETDIAG
 
-inputStr 	= ft_getopt(cfg, 'inputStr');
-outputStr   = ft_getopt(cfg, 'outputStr');
-currSubject = ft_getopt(cfg, 'currSubject');
-optionsFcn  = ft_getopt(cfg, 'optionsFcn');
-freqOutput  = ft_getopt(cfg, 'freqOutput','powandcsd');
-saveData    = ft_getopt(cfg, 'saveData');
-nTrials     = ft_getopt(cfg, 'nTrials','all');
+%%%%%% general check for inputs %%%%%%
+ntrials     = ft_getopt(cfg, 'ntrials','all');
 method      = ft_getopt(cfg, 'method');
 condition   = ft_getopt(cfg, 'condition', 'all');
+currSubject = ft_getopt(cfg, 'currSubject');
+inputStr 	= ft_getopt(cfg, 'inputStr');
+saveData    = ft_getopt(cfg, 'saveData', 'no');
+outputStr   = ft_getopt(cfg, 'outputStr');
+pathsFcn    = ft_getopt(cfg, 'pathsFcn', 'setPaths');
+optionsFcn  = ft_getopt(cfg, 'optionsFcn', 'setOptions');
 triallength = ft_getopt(cfg, 'triallength');
 
+if isempty(method)
+    error('no cfg.method given')
+end
+
+% load in data if no input data is given
 if nargin < 2
     disp(currSubject)
-    
+    eval(pathsFcn)
     eval(optionsFcn)
-    eval('setOptions')
     
     subjectFolderPath = [PATHS.SUBJECTS filesep currSubject];
     try
@@ -25,117 +76,85 @@ if nargin < 2
         connectivity = [];
         return
     end
-    
     subjectdata.cfgs.(method) = cfg;
     
 end
 
 
-
-cfg = [];
-if strcmpi(condition, 'all')
-    if strcmpi(nTrials, 'all')
-        cfg.trials = 1:length(data.trial);
-    else
-        cfg.trials = randi([1 length(data.trial)], 1, nTrials);
-    end
-else
-    itrl = find(sum(data.trialinfo==condition,2));
-    if strcmpi(nTrials, 'all')
-        cfg.trials = itrl;
-    else
-        cfg.trials = itrl(randperm(numel(itrl),nTrials));
-    end
-end
-
+%%%%%% start connectivity analysis based on given method %%%%%%
 switch(method)
-    case 'wpli_debiased'
+    case {'wpli_debiased', 'wpli'} % based on ft_connectivityanalysis with cfg.method = 'wpli_debiased'
         
-        freqLabel = {'delta', 'theta', 'alpha1', 'alpha2', 'beta', 'gamma'};
-        freqRng = {[1 3], [3 6], [6 9], [9 12], [12 25], [25, 45]};
-        
+        % cut, if needed data into trials
         if ~isempty(triallength)
-%             fprintf('\t redefining triallength to %1.0d seconds', triallength)
             cfg = [];
             cfg.saveData = 'no';
             cfg.triallength = triallength;
-            [data, finished] = bv_cutAppendedIntoTrials(cfg, data);
+            [data] = bv_cutAppendedIntoTrials(cfg, data);
         end
-            
-        fprintf('\t frequency analysis started for wpli_debiased... \n')
-        fprintf('\t \t calculation started with %1.0f trials ... ', length(data.trial))
         
+        % select data based on condition and select random trials
+        cfg = [];
+        if strcmpi(condition, 'all')
+            if strcmpi(ntrials, 'all')
+                cfg.trials = 1:length(data.trial);
+            else
+                cfg.trials = sort(randperm(length(data.trial), ntrials));
+            end
+        else
+            itrl = find(ismember(data.trialinfo, condition));
+            if strcmpi(ntrials, 'all')
+                cfg.trials = itrl;
+            else
+                cfg.trials = itrl(randperm(numel(itrl),ntrials));
+            end
+        end
+        
+        fprintf('\t connectivity analysis started for wpli_debiased... \n')
+        % frequency analysis
         cfg.method      = 'mtmfft';
         cfg.taper       = 'hanning';
-        cfg.output      = freqOutput;
+        cfg.output      = 'fourier';
         cfg.keeptrials  = 'yes';
         cfg.tapsmofrq   = 1;
         cfg.pad         = 'nextpow2';
         
-        evalc('freq            = ft_freqanalysis(cfg, data);');
-        fprintf('done! \n')
+        evalc('freq = ft_freqanalysis(cfg, data);');
         
-        fprintf('\t connectivity analysis started ... ')
+        fprintf('\t\t with %1.0f trials ... ', size(freq.trialinfo,1))
         
+        % connectivity analysis
         cfg             = [];
-        cfg.method      = 'wpli_debiased';
-        evalc('connectivitytmp = ft_connectivityanalysis(cfg, freq);');
-        
-        connectivity.freqLabel = freqLabel;
-        connectivity.freqRng = freqRng;
-        connectivity.label = freq.label;
-                
-        for iFreq = 1:length(freqRng)
-            freqStart = find(connectivitytmp.freq == freqRng{iFreq}(1));
-            freqEnd = find(connectivitytmp.freq == freqRng{iFreq}(2));
-            W = squareform(mean(...
-                connectivitytmp.wpli_debiasedspctrm(:,freqStart:freqEnd),2));
-            connectivity.wpli_debiasedspctrm(:,:,iFreq) = W;
-        end
-            
-%         connectivity.trialinfo = data.trialinfo;
-        connectivity.dimord = 'chan_chan_freq';
-        
-        fprintf('done! \n')
-        
-        if not(isempty(subjectdata.rmChannels))
-            trueRmChannels = subjectdata.rmChannels(not(ismember(subjectdata.rmChannels, OPTIONS.PREPROC.rmChannels)));
-            connectivity = addRemovedChannels(connectivity, trueRmChannels);
-        end
-        
-%         connectivity.wpli_debiasedspctrm = bv_setDiag(connectivity.wpli_debiasedspctrm, 0);
-        
-    case 'wpli'
-        
-        fprintf('\t frequency analysis started for wpli ... ')
-        
-        cfg.method      = 'mtmfft';
-        cfg.taper       = 'hanning';
-        cfg.output      = freqOutput;
-        cfg.keeptrials  = 'yes';
-        %         cfg.pad         = 'nextpow2';
-        cfg.tapsmofrq   = 2;
-        
-        evalc('freq            = ft_freqanalysis(cfg, data);');
-        fprintf('done! \n')
-        
-        fprintf('\t connectivity analysis started ... ')
-        cfg             = [];
-        cfg.method      = 'wpli';
+        cfg.method      = method;
         evalc('connectivity = ft_connectivityanalysis(cfg, freq);');
         fprintf('done! \n')
         
-        connectivity.wplispctrm = abs(connectivity.wplispctrm);
+        fnames = fieldnames(connectivity);
+        fnameindx = find(contains(fnames, 'spctrm'));
         
-        if not(isempty(subjectdata.rmChannels))
+        connectivity = renameStructField(connectivity, fnames{fnameindx}, 'spctrm');
+        
+        connectivity.method = method;
+        connectivity.label = freq.label;
+        connectivity.trialinfo = freq.trialinfo;
+        connectivity.dimord = 'chan_chan_freq';
+        
+        % find eventual removed channels and add a row of nans
+        cfg = [];
+        cfg.layout = 'biosemi32.lay';
+        cfg.skipcomnt = 'yes';
+        cfg.skipscale = 'yes';
+        evalc('layout = ft_prepare_layout(cfg);');
+        rmChannels = find(not(ismember(layout.label, connectivity.label)));
+        if not(isempty(rmChannels))
             trueRmChannels = subjectdata.rmChannels(not(ismember(subjectdata.rmChannels, OPTIONS.PREPROC.rmChannels)));
             connectivity = addRemovedChannels(connectivity, trueRmChannels);
         end
         
-%         connectivity.wplispctrm = bv_setDiag(connectivity.wplispctrm, 0);
+        % set diagonal at zero if needed
+        connectivity.spctrm = bv_setDiag(connectivity.spctrm, 0);
         
-        
-    case 'pli'
+    case 'pli' % based on PLI.m
         
         freqLabel = {'delta', 'theta', 'alpha1', 'alpha2', 'beta', 'gamma'};
         freqRng = {[1 3], [3 6], [6 9], [9 12], [12 25], [25, 45]};
@@ -152,6 +171,31 @@ switch(method)
             cfg.hpfreq = currFreqRng(1);
             
             evalc('dataFilt = ft_preprocessing(cfg, data);');
+            
+            % cut, if needed data into trials
+            if ~isempty(triallength)
+                cfg = [];
+                cfg.saveData = 'no';
+                cfg.triallength = triallength;
+                [data] = bv_cutAppendedIntoTrials(cfg, data);
+            end
+            
+            % select data based on condition and select random trials
+            cfg = [];
+            if strcmpi(condition, 'all')
+                if strcmpi(ntrials, 'all')
+                    cfg.trials = 1:length(data.trial);
+                else
+                    cfg.trials = sort(randperm(length(data.trial), ntrials));
+                end
+            else
+                itrl = find(ismember(data.trialinfo, condition));
+                if strcmpi(ntrials, 'all')
+                    cfg.trials = itrl;
+                else
+                    cfg.trials = itrl(randperm(numel(itrl),ntrials));
+                end
+            end
             
             if ~isempty(triallength)
                 cfg = [];
@@ -183,18 +227,9 @@ switch(method)
         connectivity.label = data.label;
         connectivity.trialinfo = dataCut.trialinfo;
         
-%         if isfield(subjectdata, 'rmChannels')
-%             if not(isempty(subjectdata.rmChannels))
-%                 trueRmChannels = subjectdata.rmChannels(not(ismember(subjectdata.rmChannels, OPTIONS.PREPROC.rmChannels)));
-%                 connectivity = addRemovedChannels(connectivity, trueRmChannels);
-%             end
-%         end
-%         
-%         connectivity.plispctrm = bv_setDiag(connectivity.plispctrm, 0);
-        
 end
 
-
+%%%%%% save data %%%%%%
 if strcmpi(saveData, 'yes')
     
     outputFilename = [subjectdata.subjectName '_' outputStr '.mat'];
@@ -218,7 +253,7 @@ if strcmpi(saveData, 'yes')
     
 end
 
-
+%%%%%% extra functions %%%%%%
 function connectivity = addRemovedChannels(connectivity, trueRmChannels)
 
 connectivity.label = cat(1,connectivity.label, trueRmChannels);
