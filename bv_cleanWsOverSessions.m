1d45999a2931bfdc7fc65aad648d41142149762c
Bauke van der Velde <b.vandervelde@uu.nl>
2018-09-25 14:00:17 +0200

    % added help doc

--- bv_cleanWsOverSessions.m
+++ bv_cleanWsOverSessions.m
@@ -1,4 +1,10 @@
 function output =  bv_cleanWsOverSessions(Ws)
+% usage:
+%  [ cleanedWs ] = bv_cleanWsOverSessions( Ws )
+%
+% cleans connectivity matrices over sessions, by removing channels from both
+% sessions and setting diagonal to zero.
+
 
 n = size(Ws,3);
 m = size(Ws,4);
@@ -6,20 +12,19 @@ rmChannels = [];
 output = zeros(size(Ws));
 for iW = 1:n
     currWs = squeeze(Ws(:,:,iW,:));
-    
+
     for jW = 1:m
         currW = currWs(:,:,jW);
-%         currW = normalizeW(currW);
         nanChans = find(sum(isnan(currW)) == size(currW,2));
         rmChannels = unique([rmChannels nanChans]);
         currWs(:,:,jW) = currW;
     end
-    
+
     if ~isempty(rmChannels)
         currWs(rmChannels,:,:,:) = NaN;
         currWs(:,rmChannels,:,:) = NaN;
     end
-    
+
     rmChannels = [];
     for i = 1:size(currWs,3);
         W = currWs(:,:,i);
@@ -27,7 +32,7 @@ for iW = 1:n
         W(1:ncols+1:end) = 0;
         currWs(:,:,i) = W;
     end
-    
+
     output(:,:,iW,:) = currWs;
 
 end
