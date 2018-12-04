fce4d2a8ac7f2273977ec278c6a614cf5d7416c8
Bauke van der Velde <b.vandervelde@uu.nl>
2018-12-04 12:57:26 +0100

    Add help file

--- bv_randomizeBinaryMatrices.m
+++ bv_randomizeBinaryMatrices.m
@@ -1,6 +1,13 @@
 function randomWs = bv_randomizeBinaryMatrices(Ws, m)
-% 
-% 
+% Creates a given amount of randomized binary adjacency matrices based on
+% input adjacency matrices
+%
+% usage:
+%   [Wrandomized] = bv_randomizeWeightedMatrices(Ws, m)
+%
+% With Ws being the adjecency matrices with dim(chan * chan * subject)
+% and m the amount of randomized network per W
+
 randomWs = zeros([size(Ws) m]);
 counter = 0;
 n = size(Ws,3);
@@ -15,14 +22,7 @@ for iW = 1:n
         end
         counter = counter + 1;
     end  
-    
-    if counter ~= m;
-        fprintf(repmat('\b',1,length(percStr)))
-    end
-    percDone = counter / (n * m) * 100;
-
-    percStr = sprintf('%1.0f%%', percDone);
-    fprintf([percStr '%']);
+ 
 end
 
 fprintf('\n')
